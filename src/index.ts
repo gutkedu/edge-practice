f95048769380e0fa26b26d99f6e53a45fd94aaff
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-02-09 23:28:40 -0300

    feat: practicing an simple example using upstash

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,38 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Redis } from "@upstash/redis/cloudflare";
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-}
+const redis = new Redis({
+  url: "",
+  token: "",
+});
+
+export interface Env {}
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const { pathname } = new URL(request.url);
+
+    switch (pathname) {
+      case "/write": {
+        const count = await redis.incr("count");
+
+        return new Response(JSON.stringify({ count }), {
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+      case "/read": {
+        const count = await redis.get("count");
+
+        return new Response(JSON.stringify({ count }), {
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+      default: {
+        return new Response(null, { status: 404 });
+      }
+    }
+  },
 };
